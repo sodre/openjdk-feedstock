9a1c55885902c2a9d34a8b82d013492aa4611302
Patrick Sodré <sodre@sodre.co>
2017-01-28 03:12:39 +0000

    fixing rpath

--- recipe/test-jni.sh
+++ recipe/test-jni.sh
@@ -6,6 +6,7 @@ gcc \
 	-I$JAVA_HOME/include/linux 		\
 	-L$JAVA_HOME/jre/lib/amd64/server 	\
 	-ljvm					\
+	-Wl,-rpath,'$ORIGIN/jre/lib/amd64/server'	\
 	-o vmtest				\
 	test-jni/vmtest.c 
 ./vmtest
