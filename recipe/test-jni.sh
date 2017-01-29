3e2c2f5dab02c14f0e7072cab8baae15772bbece
Patrick Sodré <sodre@sodre.co>
2017-01-29 06:41:29 +0000

    still not there.

--- recipe/test-jni.sh
+++ recipe/test-jni.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-os=$(uname -s | tr '[:upper:]' '[:lower:]')
 set -e
+os=$(uname -s | tr '[:upper:]' '[:lower:]')
+JAVA_LD_LIBRARY_PATH=$JAVA_HOME/jre/lib/amd64/server
+if [ $os == 'darwin' ]; then
+	JAVA_LD_LIBRARY_PATH=$JAVA_HOME/jre/lib/server
+fi
 gcc \
 	-I$JAVA_HOME/include 			\
 	-I$JAVA_HOME/include/$os		\
-	-L$JAVA_HOME/jre/lib/amd64/server 	\
+	-L$JAVA_LD_LIBRARY_PATH			\
 	-ljvm					\
 	-o vmtest				\
 	test-jni/vmtest.c 
-LD_LIBRARY_PATH=$JAVA_HOME/jre/lib/amd64/server ./vmtest
+LD_LIBRARY_PATH=$JAVA_LD_LIBRARY_PATH ./vmtest
