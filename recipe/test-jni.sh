7277e7ee8f678373f93349262c8edfc26b337489
Patrick Sodré <sodre@sodre.co>
2017-02-01 03:04:43 +0000

    Use dyld for darwin

--- recipe/test-jni.sh
+++ recipe/test-jni.sh
@@ -6,6 +6,7 @@ JAVA_LD_LIBRARY_PATH=$JAVA_HOME/jre/lib/amd64/server
 if [ $os == 'darwin' ]; then
 	JAVA_LD_LIBRARY_PATH=$JAVA_HOME/jre/lib/server
 fi
+
 gcc \
 	-I$JAVA_HOME/include 			\
 	-I$JAVA_HOME/include/$os		\
@@ -13,4 +14,9 @@ gcc \
 	-ljvm					\
 	-o vmtest				\
 	test-jni/vmtest.c 
-LD_LIBRARY_PATH=$JAVA_LD_LIBRARY_PATH ./vmtest
+
+if [ $os == 'darwin' ]; then
+	DYLD_LIBRARY_PATH=$JAVA_LD_LIBRARY_PATH ./vmtest
+elif [ $os == 'linux']; then
+	LD_LIBRARY_PATH=$JAVA_LD_LIBRARY_PATH ./vmtest
+fi
